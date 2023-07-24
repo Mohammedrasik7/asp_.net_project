119b7af6962a8f82af2ce234cf3e57d7c74b6f90
Rasik <mohammedrasik.shahul@elait.com>
2023-07-24 10:37:13 +0530

    client push

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,18 +6,17 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")] // /api/users
+    [Route("api/[controller]")] // GET /api/users
     public class UsersController : ControllerBase
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
         {
             _context = context;
-            
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             var users = await _context.Users.ToListAsync();
 
@@ -28,8 +27,7 @@ namespace API.Controllers
 
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            var user = await _context.Users.FindAsync(id);
-            return user;
+            return await _context.Users.FindAsync(id);
         }
     }
 }
